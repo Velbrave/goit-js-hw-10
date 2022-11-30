44aed9e8aa5538bc36a852386b18f43191522363
Vitalij Nesvyatipaska <Parkovaja60@gmail.com>
2022-11-30 19:29:58 +0200

    country

--- src/fetchCountries.js
+++
@@ -1,8 +0,0 @@
-// export fetchCountries(name){
-//   fetch(`https://restcountries.com/v3.1/name/${name}`)
-//     .then(responce => {
-//       console.log(responce);
-//       return responce.json()
-//     })
-// }
-// fetchCountries()
